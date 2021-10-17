d0e544922a5f4121f9e14b0d8a8cac05bfc0e302
Will Walker <williamwalker753@gmail.com>
2021-10-17 03:03:59 -0500

    added notification endpoint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const webpush = require('web-push')
 const { NODE_ENV } = require('./config')
 
 const app = express()
@@ -15,9 +16,28 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
+
 app.get('/', (req, res) => {
    res.send('Hello, world!')
 })
+
+app.post('/notifications/subscribe', (req, res) => {
+    const subscription = req.body;
+  
+    console.log(subscription);
+  
+    const payload = JSON.stringify({
+      title: 'Hello!',
+      body: 'It works.',
+    })
+  
+    webpush.sendNotification(subscription, payload)
+      .then(result => console.log(result))
+      .catch(e => console.log(e.stack))
+  
+    res.status(200).json({'success': true})
+});
 
 app.use(function errorHandler(error, req, res, next) {
     let response
